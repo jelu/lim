212b2fce36b3f0993b4a01fdc4bfeee98c4a830a
Jerry Lundström <lundstrom.jerry@gmail.com>
2015-08-26 13:32:57 +0200

    Release 0.20

--- lib/Lim.pm
+++ lib/Lim.pm
@@ -13,11 +13,11 @@ Lim - Framework for RESTful JSON/XML, JSON-RPC, XML-RPC and SOAP
 
 =head1 VERSION
 
-Version 0.20_07
+Version 0.20
 
 =cut
 
-our $VERSION = '0.20_07';
+our $VERSION = '0.20';
 our $CONFIG = {
     log => {
         obj_debug => 0,
